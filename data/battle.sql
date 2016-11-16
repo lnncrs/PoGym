088c22873db28713b4053e1eaa2b1fc88ca70820
lenin cristi <lenincristi@gmail.com>
2016-11-16 17:46:29 -0200

    create database & import scripts added

--- data/battle.sql
+++ data/battle.sql
@@ -1,18 +1,24 @@
 
---weakness/resistance types
-select td.type as attacker,EXP(SUM(LOG(tt.multiplier))) as multiplier
+--attack
+select td.type as defender,EXP(SUM(LOG(tt.multiplier))) as multiplier
 from types_types as tt inner join types as ta on ta.type_id = tt.attacker_type_id
 inner join types as td on td.type_id = tt.defender_type_id
-where attacker_type_id in(15,0)
+--where attacker_type_id in(15,11) --lapras
+where attacker_type_id in(16,3) --dragonite
+--where attacker_type_id in(18,0) --fairy
+--where attacker_type_id in(1,0) --fire
 group by td.type
 having EXP(SUM(LOG(tt.multiplier))) <> 1
 order by td.type
 
---weakness/resistance types
-select ta.type as defender,EXP(SUM(LOG(tt.multiplier))) as multiplier
+--defense
+select ta.type as attacker,EXP(SUM(LOG(tt.multiplier))) as multiplier
 from types_types as tt inner join types as ta on ta.type_id = tt.attacker_type_id
 inner join types as td on td.type_id = tt.defender_type_id
-where defender_type_id in(16,3)
+--where defender_type_id in(15,11) --lapras
+where defender_type_id in(16,3) --dragonite
+--where defender_type_id in(18,0) --fairy
+--where defender_type_id in(10,0) --fire
 group by ta.type
 having EXP(SUM(LOG(tt.multiplier))) <> 1
 order by ta.type
