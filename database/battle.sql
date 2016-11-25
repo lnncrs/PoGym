a2d45a120830ef5faa55a346d9a4770a44a1e1fe
Unknown <lenincristi@gmail.com>
2016-11-25 13:17:19 -0200

    pastas reorganizadas & queries montadas

--- database/battle.sql
+++ database/battle.sql
@@ -1,4 +1,6 @@
 
+use pogym;
+
 --attack
 select td.type as defender,EXP(SUM(LOG(tt.multiplier))) as multiplier
 from types_types as tt inner join types as ta on ta.type_id = tt.attacker_type_id
@@ -24,3 +26,16 @@ having EXP(SUM(LOG(tt.multiplier))) <> 1
 order by ta.type
 
 
+-- =============================================
+-- Description:
+-- Inserção de registro de ataque nas tabelas
+-- attacks_quick e attacks_charge para
+-- demostrar o disjoint entre elas
+-- =============================================
+declare @id int = 11
+select tt.type_type_id as id,ta.type as attacker,td.type as defender,tt.multiplier
+from types_types as tt inner join types as ta on ta.type_id = tt.attacker_type_id
+inner join types as td on td.type_id = tt.defender_type_id
+where attacker_type_id = @id and tt.multiplier <> 1
+order by tt.defender_type_id
+go
